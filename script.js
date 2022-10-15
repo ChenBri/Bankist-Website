ed756e30adbc638c6dbf484b65bd1871e3aa1de3
Chen Brilling <chenbrilling@gmail.com>
2022-10-15 23:17:56 +0300

    Added tab component

--- script.js
+++ script.js
@@ -32,7 +32,29 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+////////////////////////////////////////////////////////////////
 
+// Tab component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  tabsContent.forEach(tab =>
+    tab.classList.remove('operations__content--active')
+  );
+  let tab = document.querySelector(
+    `.operations__content--${clicked.dataset.tab}`
+  );
+  tab.classList.add('operations__content--active');
+  console.log(tab);
+});
 
 //////////////////////////////////////////////
 
