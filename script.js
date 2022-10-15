2cb73d5851ba10bd67eb662ba3e502f3cd764aad
Chen Brilling <chenbrilling@gmail.com>
2022-10-15 21:08:46 +0300

    Added smooth scrolling

--- script.js
+++ script.js
@@ -65,3 +65,19 @@ logo.classList.add('x');
 logo.classList.remove('x');
 logo.classList.toggle('x');
 logo.classList.contains('x');
+
+// Smooth Scroll
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth'});
+});
